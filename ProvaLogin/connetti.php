f50d695a1dc28706f221c056d4728f395d524c7d
EmanueleMelini <meliniemanuele@gmail.com>
2021-01-22 13:48:49 +0100

    Added connetti.php and updated controllologin.php

---
+++ ProvaLogin/connetti.php
@@ -0,0 +1,8 @@
+<?php
+	$conn = new mysqli("localhost", "Lele", "Lele20010205", "login");
+
+	if ($conn->connect_error)
+		die ("Errore di connessione ($conn->connect_errno), $conn->connect_error");
+	else
+		echo ("Connesso $conn->host_info  \n");
+?>
--- ProvaLogin/controllologin.php
+++ ProvaLogin/controllologin.php
@@ -4,43 +4,43 @@
               href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
               integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
               crossorigin="anonymous"/>
-        <title>Login</title>
+        <title>Controllo login</title>
     </head>
     <body>
         <table border="1">
             <tr>
                 <td>
                     <?php
+					require 'connetti.php';
+					
                     $username = $_POST["username"];
                     $password = $_POST["password"];
-                    $conn=new mysqli("localhost","root","","login");
 
-                    echo("Credenziali inserite<br>")
+                    echo ("Credenziali inserite<br>")
                         .("Username: $username<br>Password: $password")
                         .("</td>")
                         .("</tr>")
                         .("<tr>")
                         .("<td>");
 
-                    if ($conn->connect_error)
-                        die ("Errore di connessione ($conn->connect_errno), $conn->connect_error");
-                    else
-                        echo "Connesso $conn->host_info  \n";
+                    $query1 = "select * from dati";
+					$queryresult = $conn->query($query1);
+					if ($queryresult == null) {
+						echo ("Errore nella query");
+					} else {
+						echo ("<br>numero righe $queryresult->num_rows");
+						$row = $queryresult->fetch_array();
+						while ($row!=null){
+							echo ("<br><br>Username: $row[username]<br>Password: $row[password]");
+							if ($username == $row['username'] && $password == $row['password']) {
+								echo ("<br>Credenziali giuste");
+							} else {
+								echo ("<br>Credenziali errate");
+							}
 
-                    $query1="select * from dati";
-                    $queryresult=$conn->query($query1);
-                    echo "<br>numero righe $queryresult->num_rows";
-                    $row=$queryresult->fetch_array();
-                    while ($row!=null){
-                        echo("<br><br>Username: $row[username]<br>Password: $row[password]");
-                        if($username==$row['username'] && $password==$row['password']) {
-                            echo("<br>Credenziali giuste");
-                        } else {
-                            echo("<br>Credenziali errate");
-                        }
-
-                        $row=$queryresult->fetch_array();
-                    }
+							$row = $queryresult->fetch_array();
+						}
+					}
                     ?>
                 </td>
             </tr>
