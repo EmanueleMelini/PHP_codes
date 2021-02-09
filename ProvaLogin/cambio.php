af82258c8612b6051017c3fffa96c5ac3d845ef8
EmanueleMelini <meliniemanuele@gmail.com>
2021-02-09 12:45:42 +0100

    Added cambio.php and cambiopass.php

---
+++ ProvaLogin/cambio.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+    <link rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+          integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
+          crossorigin="anonymous"/>
+    <title>Controllo login</title>
+</head>
+<body>
+<table border="1">
+    <tr>
+        <td>
+            <?php
+            require 'connetti.php';
+
+            $usernameold = $_POST["usernameold"];
+            $passwordold = $_POST["passwordold"];
+            $username = $_POST["username"];
+            $password = $_POST["password"];
+
+            $query1 = "update dati set username = '".$username."', password = '".$password."' where username = '".$usernameold."' and password = '".$passwordold."'";
+            $queryresult = $conn->query($query1);
+            if (!$queryresult) {
+                echo ("Errore nella query ".$conn->error);
+            } else {
+                echo ("Username e password cambiate");
+            }
+            $query2 = "select * from dati where username = '".$username."' and password = '".$password."'";
+            $queryresult2 = $conn->query($query2);
+            if (!$queryresult2) {
+                echo ("Errore nella query ".$conn->error);
+            } else {
+                $row = $queryresult2->fetch_array();
+                if ($row!=null){
+                    echo ("<br><br>Username: $row[username]<br>Password: $row[password]");
+                }
+            }
+            echo ("<form action='login.html'>")
+                .("<input type='submit' value='Torna al login'><br>")
+                .("</form>")
+                .("<form action='cambiopass.php'>")
+                .("<input type='text' name='username' hidden value='$username'>")
+                .("<input type='text' name='password' hidden value='$password'>")
+                .("<input type='submit' value='Torna al cambio'><br>")
+                .("</form>");
+            ?>
+        </td>
+    </tr>
+</table>
+</body>
+</html>
---
+++ ProvaLogin/cambiopass.php
@@ -0,0 +1,60 @@
+<html>
+    <head>
+        <link rel="stylesheet"
+              href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+              integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
+              crossorigin="anonymous"/>
+        <title>Controllo login</title>
+    </head>
+    <body>
+        <table border="1">
+            <tr>
+                <td>
+                    <?php
+                    require 'connetti.php';
+
+                    $username = $_POST["username"];
+                    $password = $_POST["password"];
+
+                    echo ("Credenziali inserite<br>")
+                        .("Username: $username<br>Password: $password")
+                        .("</td>")
+                        .("</tr>")
+                        .("<tr>")
+                        .("<td>");
+
+                    $query1 = "select * from dati";
+                    $queryresult = $conn->query($query1);
+                    $log = false;
+                    if (!$queryresult) {
+                        echo ("Errore nella query");
+                    } else {
+                        echo ("<br>numero righe $queryresult->num_rows");
+                        $row = $queryresult->fetch_array();
+                        while ($row!=null){
+                            echo ("<br><br>Username: $row[username]<br>Password: $row[password]");
+                            if ($username == $row['username'] && $password == $row['password']) {
+                                echo ("<br>Credenziali giuste");
+                                $log = true;
+                            } else {
+                                echo ("<br>Credenziali errate");
+                            }
+
+                            $row = $queryresult->fetch_array();
+                        }
+                        if($log) {
+                            echo ("<form action='cambio.php' method='post'>");
+                            echo ("<input type='text' name='usernameold' readonly value='$username'>");
+                            echo ("<input type='text' name='passwordold' readonly value='$password'>");
+                            echo ("<input type='text' name='username' placeholder='Cambia username'>");
+                            echo ("<input type='password' name='password' placeholder='Cambia password'>");
+                            echo ("<input type='submit' name='Cambia'>");
+                            echo ("</form>");
+                        }
+                    }
+                    ?>
+                </td>
+            </tr>
+        </table>
+    </body>
+</html>
