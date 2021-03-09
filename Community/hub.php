5cb1d2f45b815b0e210f5cb8de7c8d81033e6087
EmanueleMelini <meliniemanuele@gmail.com>
2021-03-09 12:50:47 +0100

    Updated Community/ directory

--- Community/hub.php
+++ Community/hub.php
@@ -14,8 +14,8 @@ $password = $_POST['password'];
 
 $query_utente = "select * from utenti where (email = '$email' and password = '$password')";
 $queryresult_utente = $conn->query($query_utente);
-if(!$queryresult_utente) {
-    echo ("Errore nella query utente");
+if (!$queryresult_utente) {
+    echo("Errore nella query utente");
 } else {
     $row_utente = $queryresult_utente->fetch_array();
     $nome = $row_utente['nome'];
@@ -38,40 +38,49 @@ if(!$queryresult_utente) {
         $scuolaf = true;
     }
 
-    if(!$queryresult_citta) {
-        echo ("Errore nela query citta");
-    } else{
+    if (!$queryresult_citta) {
+        echo("Errore nela query citta");
+    } else {
         $cittaf = true;
     }
 
     $scuola = "Nessuna";
-    if($scuolaf) {
+    if ($scuolaf) {
         $row_scuola = $queryresult_scuola->fetch_array();
         $scuola = $row_scuola['nome'];
     }
 
     $citta = "Nessuna";
-    if($cittaf) {
+    if ($cittaf) {
         $row_citta = $queryresult_citta->fetch_array();
         $citta = $row_citta['nome'];
     }
 
     $tipos = "Nessuno";
-    if($tipo == 1) {
+    if ($tipo == 1) {
         $tipos = "Studente";
-    } else if($tipo == 2) {
+    } else if ($tipo == 2) {
         $tipos = "Docente";
-    } else if($tipo == 3) {
+    } else if ($tipo == 3) {
         $tipos = "Admin";
     }
 
-    echo ("Correttamente loggato come:<br>Nome: $nome, Cognome: $cognome, Scuola: $scuola, Citta: $citta, Tipo: $tipos, Email: $email");
+    echo("Correttamente loggato come:<br>Nome: $nome, Cognome: $cognome, Scuola: $scuola, Citta: $citta, Tipo: $tipos, Email: $email");
+
+    echo("<form action='insert_document.php' method='post'>");
+    echo("<input type='text' hidden value='$row_utente[keyu]' name='keyu'>");
+    echo("<input type='text' hidden value='$tipo' name='tipo'>");
+    echo("<input type='text' hidden value='$email' name='email'>");
+    echo("<input type='text' hidden value='$password' name='password'>");
+    echo("<input type='submit' value='Upload Documento'>");
+    echo("</form>");
 }
 ?>
 
 <form action="index.html">
     <input type="submit" value="Home">
 </form>
+
 
 </body>
 </html>
