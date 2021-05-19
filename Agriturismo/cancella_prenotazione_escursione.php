1965298178a635d40841b811638d670e8300e623
EmanueleMelini <meliniemanuele@gmail.com>
2021-05-19 15:21:03 +0200

    Added files to Exam Project Agriturismo.

---
+++ Agriturismo/cancella_prenotazione_escursione.php
@@ -0,0 +1,25 @@
+<?php
+require 'agriturismo_connect.php';
+session_start();
+$idprenescursioni = $_POST['idprenescursioni'];
+$queryeliminaprenotazione = "UPDATE PrenEscursioni SET Eliminato = 1 WHERE idPrenEscursioni = '$idprenescursioni'";
+$queryeliminaprenotazione_result = $conn->query($queryeliminaprenotazione);
+if (!$queryeliminaprenotazione_result) {
+    echo("Errore nella query");
+} else {
+    echo("Prenotazione cancellata correttamente");
+}
+?>
+<html>
+<head>
+    <title>Cancellazione Prenotazione</title>
+</head>
+<body>
+<form action="visual_prenotazioni_escursioni.php">
+    <br>Torna alla visualizzazione delle prenotazioni&nbsp;<input type="submit" value="Vai">
+</form>
+<form action="portale.php">
+    Torna al portale&nbsp;<input type="submit" value="Vai">
+</form>
+</body>
+</html>
---
+++ Agriturismo/visual_prenotazioni_attivitaippiche.php
@@ -0,0 +1,56 @@
+<?php
+require 'agriturismo_connect.php';
+session_start();
+//TODO: finire la cancellazione delle attività ippiche
+$dataoggi = date("Y-m-d");
+$oraoggi = date("H:i");
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $idprenattivitaippiche = $_POST['idprenattivitaippiche'];
+    $nome = $_POST['nome'];
+
+    echo("Vuoi cancellare davvero la prenotazione di: $nome?");
+    echo("
+<form action='cancella_prenotazione_escursione.php' method='post'>
+    <input type='submit' value='Elimina'>
+    <input type='hidden' name='idprenattivitaippiche' value='$idprenattivitaippiche'>
+</form>");
+} else {
+    $queryprenotazioni = "SELECT idPrenAttivita, idAttivita, idCliente, DataA, OraInizio, OraFine, idAddetto, AttivitaIppiche.Nome as Nomeatt, NomeCavallo, AddettiIppica.Nome, Cognome 
+FROM PrenAttivita, AttivitaIppiche, AddettiIppica
+WHERE AttivitaIppiche.idAttivitaIppiche = PrenAttivita.idPrenAttivita AND AddettiIppica.idAddettiIppica = PrenAttivita.idPrenAttivita AND DataA >= '$dataoggi' AND Eliminato = 0 AND idCliente = $_SESSION[idCliente]";
+    $queryprenotazioni_result = $conn->query($queryprenotazioni);
+    if (!$queryprenotazioni_result) {
+        echo("Errore nella query");
+    } else {
+        echo("Prenotazioni del cliente $_SESSION[Nome] $_SESSION[Cognome]<br><br>");
+        echo("<table border='1'>");
+        $row_prenotazioni = $queryprenotazioni_result->fetch_array();
+        while ($row_prenotazioni != null) {
+            echo("<tr>");
+            echo("<td>Nome Attivita: $row_prenotazioni[Nomeatt]</td>");
+            echo("<td>Nome Cavallo: $row_prenotazioni[NomeCavallo]</td>");
+            echo("<td>Addetto: $row_prenotazioni[Nome] $row_prenotazioni[Cognome]</td>");
+            echo("<td>Data Prenotazione: $row_prenotazioni[DataA]</td>");
+            echo("<form action='' method='post'>");
+            echo("<td>Cancella Prenotazione&nbsp;<input type='submit' value='Cancella'></td>");
+            echo("<input type='hidden' name='idprenattivitaippiche' value='$row_prenotazioni[idPrenAttivita]'>");
+            echo("<input type='hidden' name='nome' value='$row_prenotazioni[Nomeatt]'></form>");
+            echo("</tr>");
+            $row_prenotazioni = $queryprenotazioni_result->fetch_array();
+        }
+        echo("</table>");
+        echo("<br><br><form action=''>Cancella tutte le prenotazioni&nbsp;<input type='submit' value='Cancella'></form>");
+    }
+}
+?>
+<html>
+<head>
+    <title>Visualizza Prenotazioni</title>
+</head>
+<body>
+<form action="portale.php">
+    <br>Torna al portale&nbsp;<input type="submit" value="Vai">
+</form>
+</body>
+</html>
