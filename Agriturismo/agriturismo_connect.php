05716c0fb3154fe7c635bb716bcd58f55b48b31d
Emanuele Melini <meliniemanuele@gmail.com>
2021-05-22 23:23:54 +0200

    Delete agriturismo_connect.php

--- Agriturismo/agriturismo_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!array_key_exists("HTTP_REFERER", $_SERVER)) {
-    header("Location: http://localhost/Login/Agriturismo/hub.html");
-} else {
-    $conn = new mysqli("192.168.100.7", "e.melini", "Lele@2001", "agriturismo");
-
-    if ($conn->connect_error)
-        die ("Errore di connessione ($conn->connect_errno), $conn->connect_error");
-}
